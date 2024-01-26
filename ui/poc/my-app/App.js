489a4b83f30b45e3f3c7bcc5c4ee1ca141a4c63c
NAVINKUMAR.I <navinkumarilango0309@gmail.com>
2024-01-26 13:19:58 +0530

    UI | POC | eduapp

---
+++ ui/poc/my-app/App.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native';
+import Login from'./App/Pages/Login';
+export default function App() {
+  return (
+    <View>
+      <Login/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
